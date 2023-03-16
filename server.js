3129e44d7f3f3170b19604683cfdc47b303223d8
0mar77 <luisomar7411@gmail.com>
2023-03-15 20:23:18 -0600

    installed handlebars

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const handlebars = require('handlebars');
+
+app.engine('handlebars', handlebars({
+  layoutsDir: `${__dirname}/vies/layouts`
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('homepage', {layout: 'main'});
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
