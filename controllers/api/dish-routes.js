d6e4637e24b9df271bd5321aeb2b90e07bea9b8d
SolanoD94 <dianasolano4970@gmail.com>
2023-03-23 12:35:22 -0600

    Added helper for date formatting and user/login route renders

--- controllers/api/dish-routes.js
+++ controllers/api/dish-routes.js
@@ -1,26 +1,10 @@
 const router = require("express").Router();
 const Dish = require("../../models/Dish");
 
-// route to get one dish
-router.get("/:id", async (req, res) => {
-  try {
-    const dishData = await Dish.findByPk(req.params.id);
-    if (!dishData) {
-      res.status(404).json({ message: "No dish with this id!" });
-      return;
-    }
-    const dish = dishData.get({ plain: true });
-    res.render("card", dishData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // route to create/add a dish using async/await
 router.post("/", async (req, res) => {
   try {
     const dishData = await Dish.create(req.body);
-    // if the dish is successfully created, the new response will be returned as json
     console.log(dishData);
     res.status(200).json(dishData);
   } catch (err) {
@@ -28,4 +12,18 @@ router.post("/", async (req, res) => {
   }
 });
 
+// route to delete one dish
+router.delete("/:id", async (req, res) => {
+  try {
+    const dishData = await Dish.findByPk(req.params.id);
+    if (!dishData) {
+      res.status(404).json({ message: "No dish with this id!" });
+      return;
+    }
+    const dish = dishData.delete({ plain: true });
+    res.status(200).json(dish);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
