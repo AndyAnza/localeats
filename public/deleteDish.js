197891972ad518598192c82e556ccc3f2d4dd3e3
Andrea Ramirez <andrea.anza3@gmail.com>
2023-03-24 03:09:19 -0600

    Fixed bug on DELETE user/id/dishId

--- public/deleteDish.js
+++ public/deleteDish.js
@@ -1,23 +1,48 @@
-const deleteDishBtn = document.getElementById("deleteDishBtn");
+// const deleteDishBtn = document.getElementById("deleteDishBtn");
 
-deleteDishBtn.addEventListener("click", async (event) => {
-  const dishId = event.target.getAttribute("data-dish-id");
-  const userId = event.target.getAttribute("data-user-id");
+// deleteDishBtn.addEventListener("click", async (event) => {
+//   const dishId = event.target.getAttribute("data-dish-id");
+//   const userId = event.target.getAttribute("data-user-id");
 
-  console.log(dishId);
-  console.log(userId);
+//   console.log(dishId);
+//   console.log(userId);
 
-  // Make a DELETE request to delete the dish with the given ID and user ID
-  const response = await fetch(`/user/${userId}/${dishId}`, {
-    method: "DELETE",
+//   // Make a DELETE request to delete the dish with the given ID and user ID
+//   const response = await fetch(`/user/${userId}/${dishId}`, {
+//     method: "DELETE",
+//   });
+
+//   // Handle the response
+//   if (response.ok) {
+//     // Reload the page to reflect the changes
+//     window.location.reload();
+//   } else {
+//     const data = await response.json();
+//     console.error(data.error);
+//   }
+// });
+const deleteDishBtns = document.querySelectorAll(".deleteDishBtn");
+
+deleteDishBtns.forEach((deleteDishBtn) => {
+  deleteDishBtn.addEventListener("click", async (event) => {
+    const dishId = event.target.getAttribute("data-dish-id");
+    const userId = event.target.getAttribute("data-user-id");
+
+    console.log(dishId);
+    console.log(userId);
+
+    // Make a DELETE request to delete the dish with the given ID and user ID
+    const response = await fetch(`/user/${userId}/${dishId}`, {
+      method: "DELETE",
+    });
+
+    // Handle the response
+    if (response.ok) {
+      // Reload the page to reflect the changes
+      window.location.reload();
+    } else {
+      const data = await response.json();
+      console.error(data.error);
+    }
   });
-
-  // Handle the response
-  if (response.ok) {
-    // Reload the page to reflect the changes
-    window.location.reload();
-  } else {
-    const data = await response.json();
-    console.error(data.error);
-  }
 });
