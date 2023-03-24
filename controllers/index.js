bd932e37a654968c1394533d687e72dd2a9f10b0
SolanoD94 <dianasolano4970@gmail.com>
2023-03-23 23:46:12 -0600

    timeline dashboard tryout

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,11 @@
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
-const dashboardRoutes = require("./dashboard");
 const userRoutes = require("./user-routes");
+const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
 router.use("/user", userRoutes);
 router.use("/api", apiRoutes);
-router.use("/my-dashboard", dashboardRoutes);
 
 module.exports = router;
