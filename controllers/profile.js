5a26b53eea2699988e6d927b5e9b6cfdbf3ec219
SolanoD94 <dianasolano4970@gmail.com>
2023-03-25 14:39:13 -0600

    get routes and front-end links all working

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 const { User, Dish } = require("../models");
 
-//Get route profile/new
-router.get("/new", async (req, res) => {
+//GET route to signup
+router.get("/signup", async (req, res) => {
+  try {
+    const newUserData = await User.findAll({});
+    const newUser = newUserData.map((user) => user.get({ plain: true }));
+    console.log(newUser);
+    res.render("pages/createaccount", { newUser });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET route to profile settings
+router.get("/my-settings", async (req, res) => {
   try {
     const newUserData = await User.findAll({});
     const newUser = newUserData.map((user) => user.get({ plain: true }));
@@ -10,25 +22,6 @@ router.get("/new", async (req, res) => {
     res.render("profile", { newUser });
   } catch (err) {
     res.status(500).json(err);
-  }
-});
-
-router.post("/new", async (req, res) => {
-  try {
-    const { user_name, email, phone, location, password } = req.body;
-    if (!user_name || !email || !password) {
-      return res.status(400).json({ error: "Missing required properties" });
-    }
-    const newUser = await User.create({
-      user_name,
-      email,
-      phone,
-      location,
-      password,
-    });
-    res.status(202).json({ message: "New User created", user: newUser });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
   }
 });
 
