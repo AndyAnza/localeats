0523f54899e593f379cef9c99c277edebe67342d
Andrea Ramirez <andrea.anza3@gmail.com>
2023-03-27 21:36:38 -0600

    second push

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  "heroku_6ce4b3372970715",
-  "b520a2a267663c",
-  "ff55d43d",
-  {
-    host: "us-cdbr-east-06.cleardb.net",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
 sequelize
   .authenticate()
   .then(() => {
